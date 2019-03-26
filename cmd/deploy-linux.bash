654106ccf1cc66803630dc63fd5b3c2cedc7b370
chulo <emilio.astarita@gmail.com>
2019-03-26 07:03:15 -0300

    Gen release for snap

--- cmd/deploy-linux.bash
+++ cmd/deploy-linux.bash
@@ -1,30 +1,23 @@
 #!/usr/bin/env bash
 
 DEPLOY_DIR=deploy/linux
-DEBIAN_RELEASE=${DEPLOY_DIR}/release
-DEBIAN_BIN=${DEBIAN_RELEASE}/usr/bin
-DEBIAN_ICONS=${DEBIAN_RELEASE}/usr/share/icons/hicolor
-DEBIAN_APPLICATIONS=${DEBIAN_RELEASE}/usr/share/applications
+RELEASE_DIR=deploy/lyricfier
+ICONS=${DEPLOY_DIR}/icons/
 rm -rf ${DEPLOY_DIR}
 qtdeploy build desktop
 
-# debian dir
-mkdir -p ${DEBIAN_RELEASE}/DEBIAN
-cp debian-control ${DEBIAN_RELEASE}/DEBIAN/control
 # bin
-mkdir -p ${DEBIAN_BIN}
-cp ${DEPLOY_DIR}/cmd ${DEBIAN_BIN}/lyricfier
+mv ${DEPLOY_DIR}/cmd ${DEPLOY_DIR}/lyricfier
 # desktop file
-mkdir -p ${DEBIAN_APPLICATIONS}
-cp lyricfier.desktop ${DEBIAN_APPLICATIONS}
+cp lyricfier.desktop ${DEPLOY_DIR}
 # icon
-
 declare -a SIZES=("16x16" "24x24" "32x32" "48x48" "22x22" "128x128" "256x256")
 
 for SIZE in ${SIZES[@]}; do
-   mkdir -p ${DEBIAN_ICONS}/${SIZE}/apps/
-   convert assets/icon.png -resize ${SIZE}\> ${DEBIAN_ICONS}/${SIZE}/apps/lyricfier.png
+   mkdir -p ${ICONS}/${SIZE}/
+   convert assets/icon.png -resize ${SIZE}\> ${ICONS}/${SIZE}/lyricfier.png
 done
 
-# Create release
-dpkg-deb --build ${DEBIAN_RELEASE}
+mv ${DEPLOY_DIR}  ${RELEASE_DIR}
+tar -zcvf release.tar.gz -C ${RELEASE_DIR}/../ lyricfier
+
